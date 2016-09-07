d68e9b4508a15771db941f0ffbdb9829ca0d565e
Aleksey Ivanov <ialexxei@gmail.com>
2016-09-07 18:21:29 +0300

    remove unnecessary things

--- features/step_definitions/index_steps.rb
+++
@@ -1,8 +0,0 @@
-Given /^I visit the "(.+)" page$/ do |url|
-	  visit(url)
-end
-
-Then /^I see "(.*)" text$/ do |text|
-	  response_body.should contain(/#{text}/m)
-end
-
--- spec/spec_helper.rb
+++
@@ -1,7 +0,0 @@
-require 'rubygems'
-require 'sinatra'
-require 'rack/test'
-require 'spec'
-
-# set test environment
-set :environment, :test
